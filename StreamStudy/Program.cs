c2cffdf1d1ebe772a396f70868ee5fadc00435a0
XS <1194889303@qq.com>
2020-09-22 21:38:56 +0800

    IO操作,递归遍历所有文件

---
+++ StreamStudy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace StreamStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemInfo dir=new DirectoryInfo(@"F:\111");
+            StreamHelper.FileSearch(dir);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StreamStudy/StreamHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace StreamStudy
+{   /// <summary>
+    /// 处理IO，流的帮助类
+    /// </summary>
+    class StreamHelper
+    {
+        public static void FileSearch(FileSystemInfo fileSystemInfo)
+        {
+            if (!fileSystemInfo.Exists)
+            {
+                return;
+            }
+            //不是目录
+            DirectoryInfo drInfo = fileSystemInfo as DirectoryInfo;
+            if (drInfo == null)
+            {
+                return;
+            }
+            FileSystemInfo[] fileInfos = drInfo.GetFileSystemInfos();
+            foreach (var item in fileInfos)
+            {
+                FileInfo file = item as FileInfo;
+                //是文件
+                if (file != null)
+                {
+                    Console.WriteLine("文件:" + item.Name + "路径:" + item.FullName);
+                }
+                //是目录
+                else
+                {
+                    //递归
+                    FileSearch(item);
+                }
+            }
+        }
+    }
+}
